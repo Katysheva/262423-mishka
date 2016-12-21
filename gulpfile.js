823737d111aafde150647febfb5ed351db5bc92f
Polina Katysheva <polly995@yandex.ru>
2016-12-21 22:26:15 +0300

    automatisation

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ var server = require("browser-sync").create();
 var rename = require("gulp-rename");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+
+var imagemin = require("gulp-imagemin");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -17,9 +21,16 @@ gulp.task("style", function() {
   .pipe(postcss([
     autoprefixer({browsers: [
       "last 2 versions"
-      ]})
+      ]}),
+    mqpacker({
+      sort: true
+    })
     ]))
   .pipe(gulp.dest("css"))
+  .pipe(minify())
+  .pipe(rename("style.min.css"))
+  .pipe(gulp.dest("css"))
+
   .pipe(server.stream());
 });
 
@@ -44,4 +55,35 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+  .pipe(imagemin([
+   imagemin.optipng({optimizationLevel: 3}),
+   imagemin.jpegtran({progressive: true})
+   ]))
+  .pipe(gulp.dest("img"));
+});
+
+gulp.task("copy", function() {
+ return gulp.src([
+   "fonts/**/*.{woff,woff2}",
+   "img/**",
+   "js/**",
+   "*.html"
+   ], {
+     base: "."
+   })
+ .pipe(gulp.dest("build"));
+});
+
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "images", "symbols", fn);
+});
+
+var del = require("del");
+gulp.task("clean", function() {
+  return del("build");
 });
