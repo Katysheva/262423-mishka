c7611987dc3997d43c740b7b27bfca3ae86d3bce
Polina Katysheva <polly995@yandex.ru>
2016-12-23 17:55:49 +0300

    Fixes. Modal

--- js/app.js
+++ js/app.js
@@ -11,32 +11,39 @@ function openMenu() {
   }
 }
 
-function openModal() {
-  var overlay = $('#overlay');
-  var open_modal = $('.open_modal');
-  var close = $('#overlay');
-  var modal = $('.modal-window');
-  var body = $('body');
+(function($) {
 
-  open_modal.click( function(event){
-    event.preventDefault();
-    var div = $(this).attr('href');
-    overlay.fadeIn(400,
-      function(){
-        $(div)
-        .css('display', 'block')
-        .animate({opacity: 1, top: '50%'}, 200);
-      });
-    $(body).css('overflow', 'hidden');
-    $(modal).css('overflow', 'auto');
-  });
-  close.click( function(){
-    modal
-    .animate({opacity: 0, top: '45%'}, 200,
-      function(){
-        $(this).css('display', 'none');
-        overlay.fadeOut(400);
-      });
-    $(body).css('overflow', 'auto');
-  });
-}
+  $(document).ready(function() {
+
+    var overlay = $('#overlay');
+    var open_modal = $('.open_modal');
+    var close = $('#overlay');
+    var modal = $('.modal-window');
+    var body = $('body');
+
+    open_modal.click( function(event){
+      event.preventDefault();
+      var div = $(this).attr('href');
+      overlay.fadeIn(400,
+        function(){
+          $(div)
+          .css('display', 'block')
+          .animate({opacity: 1, top: '50%'}, 200);
+        });
+      $(body).css('overflow', 'hidden');
+      $(modal).css('overflow', 'auto');
+    });
+    close.click( function(){
+      modal
+      .animate({opacity: 0, top: '45%'}, 200,
+        function(){
+          $(this).css('display', 'none');
+          overlay.fadeOut(400);
+        });
+      $(body).css('overflow', 'auto');
+    });
+
+
+  })
+
+})(jQuery)
